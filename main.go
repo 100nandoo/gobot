4fa9551b3a212ff413e5573e27194d9e7236c90b
Fernando <100nandoo@gmail.com>
2026-05-16 19:51:34 +0800

    Bump version to v1.9.27

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 )
 
 func main() {
-	pkg.LogWithTimestamp("Gobot v1.9.26 started...")
+	pkg.LogWithTimestamp("Gobot v1.9.27 started...")
 	freegames.Scouting(false)
 	freegames.Cleaning(false)
 
